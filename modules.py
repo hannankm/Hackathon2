284ee0649cb2a809856d165b0dc0c51c1ad31d46
lydiafikire05@gmail.com <youremail@example.com>
2024-05-10 21:50:24 +0300

    Initial commi

--- modules.py
+++ modules.py
@@ -19,15 +19,25 @@ def login_required(f):
     return decorated_function
 
 
-def get_weather(latitude, longitude, api_key):
-    weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
+import requests
+
+
+def get_weather(lat, lon, api_key):
+    cnt = 7  # Specify the number of forecast data points you want
+    weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
     response = requests.get(weather_url)
     if response.status_code == 200:
         weather_data = response.json()
-        return {
-            "temperature": weather_data["main"]["temp"],
-            "humidity": weather_data["main"]["humidity"],
-            "wind_speed": weather_data["wind"]["speed"],
-        }
+        try:
+            temperature = weather_data["main"]["temp"]
+            humidity = weather_data["main"]["humidity"]
+            wind_speed = weather_data["wind"]["speed"]
+            return {
+                "temperature": temperature,
+                "humidity": humidity,
+                "wind_speed": wind_speed,
+            }
+        except KeyError as e:
+            return {"error": f"KeyError: {e}"}
     else:
         return {"error": "Failed to fetch weather data"}
